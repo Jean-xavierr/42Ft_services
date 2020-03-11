1d09bf7654ab556c5cbbbefda63b64f11579848c
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-03-11 14:35:03 +0100

    :whale: feat: Dockerfile Alpine_ftps | srcs | script_init
    1) Dockerfile Alpine ftps server
    2) script_init_ftp.sh [for init ftps server]

--- script.sh
+++
@@ -1,9 +0,0 @@
-brew cask install virtualbox
-brew install kubectl
-brew install minikube
-mv ./minikube /goinfre/jereligi
-cd /goinfre/jereligi
-ln -s /goinfre/jereligi/.minikube /Users/jereligi/.minikube
-minikube start --vm-driver=virtualbox --disk-size=5000MB
-
-
---
+++ srcs/script_init_ftp.sh
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    script_init_ftp.sh                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jereligi <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/03/11 14:31:51 by jereligi          #+#    #+#              #
+#    Updated: 2020/03/11 14:31:57 by jereligi         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+mkdir /src/ftp
+mkdir /src/ftp/jereligi
+echo -e "test2018\ntest2018" | adduser -h /src/ftp/jereligi jereligi
+mkdir -p /src/ftp/jereligi
+chown jereligi /src/ftp/jereligi
+mkdir /src/ftp/jereligi/folder_text
+touch /src/ftp/jereligi/folder_text/text.txt
+
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=127.0.0.1 /etc/vsftpd/vsftpd.conf
