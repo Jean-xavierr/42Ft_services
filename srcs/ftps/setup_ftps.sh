63a0190fc1e4a4c5ddf59b6b6de66527dd260a9e
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-08-24 17:30:28 +0200

    🐳 feat: Dockerfile ftps | config files
    - Add Dockerfile ftps
    - Add setup_ftps.sh
    - Add vsftpd.conf
    
    - Update Dockerfile Nginx

--- srcs/ftps/setup_ftps.sh
+++ srcs/ftps/setup_ftps.sh
@@ -1,19 +1,22 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    script_init_ftp.sh                                 :+:      :+:    :+:    #
+#    setup_ftps.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jereligi <marvin@42.fr>                    +#+  +:+       +#+         #
+#    By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/11 14:31:51 by jereligi          #+#    #+#              #
-#    Updated: 2020/03/12 14:39:48 by jereligi         ###   ########.fr        #
+#    Updated: 2020/08/24 17:28:44 by jereligi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-echo -e "test2018\ntest2018" | adduser -h /src/ftp/jereligi jereligi
-mkdir -p /src/ftp/jereligi
-chown jereligi /src/ftp/jereligi
-mkdir /src/ftp/jereligi/folder_text
-touch /src/ftp/jereligi/folder_text/text.txt
+echo -e "$PASSWORD\n$PASSWORD" | adduser -h /src/ftp/$USER $USER
+mkdir -p /src/ftp/$USER
+chown $USER:$USER /src/ftp/$USER
+mkdir /src/ftp/$USER/folder_text
+chown $USER:$USER /src/ftp/$USER/folder_text
+touch /src/ftp/$USER/folder_text/text.txt
 
-exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=127.0.0.1 /etc/vsftpd/vsftpd.conf
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=127.0.0.1 /etc/vsftpd/vsftpd.conf &
+tail -f /dev/null
+
