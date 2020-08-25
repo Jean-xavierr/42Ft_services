fd3d110dde9a744b9ea49118efcfe9dc8c12e8d5
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-08-25 18:34:27 +0200

    🐳 feat: Add Dockerfile MySQL | setup.sh

--- srcs/mysql/setup.sh
+++ srcs/mysql/setup.sh
@@ -1,7 +1,20 @@
+# Config Openrc and start Mariadb
 openrc
 touch /run/openrc/softlevel
 /etc/init.d/mariadb setup
 rc-service mariadb start
-mysql -u root < /srcs/create_database
+
+# Create Database wordpress
+mysql -u root << EOF 
+CREATE DATABASE $WP_DB_NAME;
+CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$PASSWORD';
+GRANT ALL ON $WP_DB_NAME.* TO '$WP_USER'@'%' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+# Comment line start with 'skip-networking'
+mv /etc/my.cnf.d/mariadb-server.cnf /etc/my.cnf.d/old_mariadb-server.cnf
+sed 's/^skip-networking/#&/' /etc/my.cnf.d/old_mariadb-server.cnf > /etc/my.cnf.d/mariadb-server.cnf
+rm /etc/my.cnf.d/old_mariadb-server.cnf
 
 tail -f /dev/null
