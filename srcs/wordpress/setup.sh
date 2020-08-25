87c937e5505c45cf66778ec61e77cfc427f32e3b
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-08-25 18:31:20 +0200

    🐳 feat: Udapte Dockerfile and config WordPress

--- srcs/wordpress/setup.sh
+++ srcs/wordpress/setup.sh
@@ -1,1 +1,36 @@
-php -S 0.0.0.0:5050 -t /www/
+# Config wp-config.php (database mariadb)
+cat << EOF > wp-config.php
+<?php
+# Config Mariadb | database
+
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wp_user');
+define('DB_PASSWORD', '$PASSWORD');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('FS_METHOD', 'direct');
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+\$table_prefix = 'wp_';
+
+define('WP_DEBUG', false);
+/* C’est tout, ne touchez pas à ce qui suit ! Bonne publication. */
+/** Chemin absolu vers le dossier de WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+/** Réglage des variables de WordPress et de ses fichiers inclus. */
+require_once(ABSPATH . 'wp-settings.php');
+?>
+EOF
+
+# PHP SERVER
+php -S 0.0.0.0:5050 -t /www/wordpress
