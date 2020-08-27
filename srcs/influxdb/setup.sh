82a537505fc4148c012d44d0ce8d46bcce337116
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-08-27 14:46:33 +0200

    🐳 feat: Dockerfile InfluxDB | config files

---
+++ srcs/influxdb/setup.sh
@@ -0,0 +1,9 @@
+openrc
+touch /run/openrc/softlevel
+service influxdb start 
+sleep 3
+influx << EOF
+CREATE DATABASE $INFLUXDB_NAME;
+CREATE USER "$INFLUXDB_USER" WITH PASSWORD '$PASSWORD' WITH ALL PRIVILEGES;
+GRANT ALL ON $INFLUXDB_NAME TO $INFLUXDB_USER;
+EOF
