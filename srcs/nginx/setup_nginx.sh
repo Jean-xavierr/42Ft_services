0bd914d3c56a30dc3c82c2f639c22cf9b5a42075
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-08-31 14:23:13 +0200

    :arrow_up: feat : Add ssh to Nginx | Update Dockerfile and .yaml

--- srcs/nginx/setup_nginx.sh
+++ srcs/nginx/setup_nginx.sh
@@ -2,5 +2,10 @@
 openrc
 touch /run/openrc/softlevel
 service nginx start
+echo -e "$PASSWORD\n$PASSWORD" | adduser $USER
+/usr/bin/ssh-keygen -A
+rc-update add sshd
+rc-status
+/etc/init.d/sshd start
 
 tail -f /dev/null # Freeze command to avoid end of container
