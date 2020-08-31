c2fb8ab6907ae7501cc86c00aaa4d318de03fa90
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-08-31 18:37:43 +0200

    🌐 feat: Update mysql-deployment.yaml

--- srcs/mysql/setup.sh
+++ srcs/mysql/setup.sh
@@ -1,3 +1,8 @@
+# Comment line start with 'skip-networking'
+mv /etc/my.cnf.d/mariadb-server.cnf /etc/my.cnf.d/old_mariadb-server.cnf
+sed 's/^skip-networking/#&/' /etc/my.cnf.d/old_mariadb-server.cnf > /etc/my.cnf.d/mariadb-server.cnf
+rm /etc/my.cnf.d/old_mariadb-server.cnf
+
 # Config Openrc and start Mariadb
 openrc
 touch /run/openrc/softlevel
@@ -11,10 +16,5 @@ CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$PASSWORD';
 GRANT ALL ON $WP_DB_NAME.* TO '$WP_USER'@'%' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
-
-# Comment line start with 'skip-networking'
-mv /etc/my.cnf.d/mariadb-server.cnf /etc/my.cnf.d/old_mariadb-server.cnf
-sed 's/^skip-networking/#&/' /etc/my.cnf.d/old_mariadb-server.cnf > /etc/my.cnf.d/mariadb-server.cnf
-rm /etc/my.cnf.d/old_mariadb-server.cnf
 
 tail -f /dev/null
