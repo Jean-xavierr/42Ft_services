9a837323d6f8b62684e30498fc312a4f58cb0427
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-08-31 11:25:37 +0200

    🌐 feat: Add ftps-deployment.yaml

--- srcs/ftps/setup_ftps.sh
+++ srcs/ftps/setup_ftps.sh
@@ -6,7 +6,7 @@
 #    By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/11 14:31:51 by jereligi          #+#    #+#              #
-#    Updated: 2020/08/24 17:28:44 by jereligi         ###   ########.fr        #
+#    Updated: 2020/08/31 11:24:01 by jereligi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,6 +17,4 @@ mkdir /src/ftp/$USER/folder_text
 chown $USER:$USER /src/ftp/$USER/folder_text
 touch /src/ftp/$USER/folder_text/text.txt
 
-exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=127.0.0.1 /etc/vsftpd/vsftpd.conf &
-tail -f /dev/null
-
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=$ADRESS /etc/vsftpd/vsftpd.conf &
