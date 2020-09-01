ee9e39c1694d4df2a56d63758de37996914114cd
Jean-xavier <religieuxjxavier@gmail.com>
2020-09-01 16:28:35 +0200

    ⬆️ feat : Update setup.sh FTPS

--- srcs/ftps/setup_ftps.sh
+++ srcs/ftps/setup_ftps.sh
@@ -3,18 +3,19 @@
 #                                                         :::      ::::::::    #
 #    setup_ftps.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+         #
+#    By: Jeanxavier <Jeanxavier@student.42.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/11 14:31:51 by jereligi          #+#    #+#              #
-#    Updated: 2020/08/31 11:24:01 by jereligi         ###   ########.fr        #
+#    Updated: 2020/09/01 16:15:28 by Jeanxavier       ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-echo -e "$PASSWORD\n$PASSWORD" | adduser -h /src/ftp/$USER $USER
-mkdir -p /src/ftp/$USER
-chown $USER:$USER /src/ftp/$USER
-mkdir /src/ftp/$USER/folder_text
-chown $USER:$USER /src/ftp/$USER/folder_text
-touch /src/ftp/$USER/folder_text/text.txt
+echo -e "$PASSWORD\n$PASSWORD" | adduser -h /srcs/ftp/$USER $USER
+mkdir -p /srcs/ftp/$USER
+chown $USER:$USER /srcs/ftp/$USER
+mkdir /srcs/ftp/$USER/folder_text
+chown $USER:$USER /srcs/ftp/$USER/folder_text
+touch /srcs/ftp/$USER/folder_text/text.txt
 
 exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=$ADRESS /etc/vsftpd/vsftpd.conf &
+tail -f /dev/null
