9e4230f3154e51180c0cf11b2fe5391995affb4f
Jean-xavier <religieuxjxavier@gmail.com>
2020-09-01 14:53:59 +0200

    ⬆️ feat : Update setup.sh Nginx | SSH config

--- srcs/nginx/setup_nginx.sh
+++ srcs/nginx/setup_nginx.sh
@@ -3,7 +3,6 @@ openrc
 touch /run/openrc/softlevel
 service nginx start
 echo -e "$PASSWORD\n$PASSWORD" | adduser $USER
-/usr/bin/ssh-keygen -A
 rc-update add sshd
 rc-status
 /etc/init.d/sshd start
