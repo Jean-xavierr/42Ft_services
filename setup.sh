2f75d59f41405f979da0cf9747a72d80b345022b
Jean-xavier <religieuxjxavier@gmail.com>
2020-09-02 13:30:12 +0200

    :bug: fix : Error Metallb with external ip

--- setup.sh
+++ setup.sh
@@ -136,6 +136,7 @@ function_install_metallb()
 {
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 }
 
 function_management_install()
