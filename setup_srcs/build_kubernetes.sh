65a6dd5db0a4baad4fb5e4f00fb94f4ea46a9df8
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-09-07 10:27:16 +0200

    :bug: fix : LivenessProbe [FTPS/Wordpress/Phpmyadmin]

--- setup_srcs/build_kubernetes.sh
+++ setup_srcs/build_kubernetes.sh
@@ -34,6 +34,7 @@ start_minikube()
 
 install_addons_minikube()
 {
+	sleep 2
 	minikube addons enable metrics-server
 	minikube addons enable logviewer
 }
@@ -106,7 +107,7 @@ display_service()
 	printf ":5050  |\n"
 	echo " ---------------------------------------------------------------------------------------"
 	printf "| ${Yellow}PhpMyAdmin${Default_color}     | user: wp_user   | password: password | ip: http://"
-	kubectl get svc | grep phpmyadmin-service | cut -d " " -f 10,11,12 | tr -d "\n" | tr -d " "
+	kubectl get svc | grep phpmyadmin-service | cut -d " " -f 10,11,12,13 | tr -d "\n" | tr -d " "
 	printf ":5000  |\n"
 	echo " ---------------------------------------------------------------------------------------"
 	printf "| ${Green}Ftps${Default_color}           | user: ftps_user | password: password | ip: "
